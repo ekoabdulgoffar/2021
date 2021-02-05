1252f16da9f74840ad4a10160fc5ce2f9caaca3c
wulanfrom <biasindi.wulanfrom@gmail.com>
2021-02-06 00:32:54 +0900

    organizers and reg fix reg table still not working

--- app.js
+++ app.js
@@ -85,34 +85,45 @@ window.onload = function() {
         });
     }
 
-    const keyboardControl = () => {
-        $("nav:first").accessibleMegaMenu({
-            /* prefix for generated unique id attributes, which are required 
-               to indicate aria-owns, aria-controls and aria-labelledby */
-            uuidPrefix: "accessible-megamenu",
-
-            /* css class used to define the megamenu styling */
-            menuClass: "nav-menu",
-
-            /* css class for a top-level navigation item in the megamenu */
-            topNavItemClass: "nav-item",
-
-            /* css class for a megamenu panel */
-            panelClass: "sub-nav",
-
-            /* css class for a group of items within a megamenu panel */
-            panelGroupClass: "sub-nav-group",
-
-            /* css class for the hover state */
-            hoverClass: "hover",
-
-            /* css class for the focus state */
-            focusClass: "focus",
-
-            /* css class for the open state */
-            openClass: "open"
+    const confTable = () => {
+        const tableConf = document.querySelector('.conf-rates');
+        const thConf = tableConf.querySelectorAll('thead th');
+        const tdConfLabel = Array.from(thConf).map(el => el.innerText);
+        tableConf.querySelectorAll('tbody tr').forEach( tr => {
+            Array.from(tr.children).forEach( 
+            (td, ndx) =>  td.setAttribute('label', tdConfLabel[ndx])
+            );
         });
     }
+
+    // const keyboardControl = () => {
+    //     $("nav:first").accessibleMegaMenu({
+    //         /* prefix for generated unique id attributes, which are required 
+    //            to indicate aria-owns, aria-controls and aria-labelledby */
+    //         uuidPrefix: "accessible-megamenu",
+
+    //         /* css class used to define the megamenu styling */
+    //         menuClass: "nav-menu",
+
+    //         /* css class for a top-level navigation item in the megamenu */
+    //         topNavItemClass: "nav-item",
+
+    //         /* css class for a megamenu panel */
+    //         panelClass: "sub-nav",
+
+    //         /* css class for a group of items within a megamenu panel */
+    //         panelGroupClass: "sub-nav-group",
+
+    //         /* css class for the hover state */
+    //         hoverClass: "hover",
+
+    //         /* css class for the focus state */
+    //         focusClass: "focus",
+
+    //         /* css class for the open state */
+    //         openClass: "open"
+    //     });
+    // }
     
     const app = () => {
         navSlide();
@@ -121,7 +132,8 @@ window.onload = function() {
         triggerLoad();
         goToCfpEn();
         setMobileTable();
-        keyboardControl();
+        confTable();
+         // keyboardControl();
     }
     
     app()
